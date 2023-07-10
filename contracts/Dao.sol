0458dca44a9438c6086a9d549b6fcecb102b15f7
ankitkumar <lostankit7@gmail.com>
2023-07-10 22:21:21 +0530

    inital commit for dao

---
+++ contracts/Dao.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/secuiry/ReentrancyGuard.sol";
+
+contract Dao is ReentrancyGuard, AccessControl{
+    bytes32 
+}
---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks:{
+    hardhat:{
+      chainId:1337
+    }
+  },
+  settings:{
+    optimizer:{
+      enabled:true,
+      run:200
+    }
+  }
+};
